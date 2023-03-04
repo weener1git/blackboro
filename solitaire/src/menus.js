3e07c8f9133c45b98b6d130b83eb7bbac46c1db2
blackboro <goran.spu@gmail.com>
2023-03-04 11:00:48 +0100

    Update menus.js

--- solitaire/src/menus.js
+++ solitaire/src/menus.js
@@ -80,7 +80,7 @@ var menus = {
 		{
 			item: "&About Solitaire...",
 			action: ()=> {
-				window.open("https://github.com/3kh0/3kh0.github.io/issues/103");
+				window.open("https://77games.io");
 			},
 			description: "About Solitaire"
 		},
